a4b3e5f8ba3e7adf6aaac731a71eea15783f79d4
Nikita Lyubchich <cybran111@gmail.com>
2015-08-24 19:23:30 +0300

    Working on

--- frontend/js/questionnaire.js
+++ frontend/js/questionnaire.js
@@ -7,6 +7,10 @@ require("domready")(function () {
     var Asker = React.createClass({
         displayName: "Asker",
 
+        getSource: function (sourceList) {
+            return sourceList.join(", ")
+        },
+
         changeCurrentWord: function () {
             this.setState({currentEl: _.sample(this.state.words)})
         },
@@ -21,28 +25,31 @@ require("domready")(function () {
             if (React.findDOMNode(this.refs.guessInput).value === this.state.currentEl.target) {
                 _.pull(this.state.words, this.state.currentEl);
                 if (this.state.words.length !== 0){
+                    this.setState({status: "OK"});
                     this.changeCurrentWord();
                     console.log("success")
                 } else {
                     this.setState({status: "Finished!"})
                 }
             } else {
+                this.setState({status: "FAIL! Word: "+this.getSource(this.state.currentEl.source)+" Expected:"+this.state.currentEl.target+"; Actual: "+React.findDOMNode(this.refs.guessInput).value});
                 this.changeCurrentWord();
                 console.log("fail")
             }
+            React.findDOMNode(this.refs.guessInput).value = ""
         },
         
         inputKeyPress: function (e) {
             if (e.which == '13'){ // enter
                 this.checkWord();
-                React.findDOMNode(this.refs.guessInput).value = ""
             }
         },
 
         render: function () {
             return React.DOM.div(null,
-                React.DOM.p(null, "Status "+this.state.status),
-                React.DOM.p(null, this.state.currentEl.source.join(", ")),
+                React.DOM.p(null, "Remaining: "+this.state.words.length),
+                React.DOM.p(null, this.state.status),
+                React.DOM.p(null, this.getSource(this.state.currentEl.source)),
                 React.DOM.input({type: "text", ref: "guessInput", onKeyPress: this.inputKeyPress}),
                 React.DOM.button({onClick: this.checkWord}, "Guess")
             )
