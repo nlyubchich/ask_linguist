9b613b4b323d4f18b61982bef002245f988a0e3d
Nikita Lyubchich <nikita.lyubchich@gmail.com>
2016-07-24 21:53:52 +0300

    trying to use whitenoise

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn project:app
+web: gunicorn project:whitenoise_app
--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,7 @@
+from whitenoise import WhiteNoise
+
 from project.application import create_app
 
 app = create_app()
+
+whitenoise_app = WhiteNoise(app, './static')
