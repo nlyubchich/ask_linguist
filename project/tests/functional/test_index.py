c21fa3a21689a7bc17dd76379d57016e21e571f2
Nikita Lyubchich <nikita.lyubchich@gmail.com>
2016-08-12 19:57:21 +0300

    fixed test

--- project/tests/functional/test_index.py
+++ project/tests/functional/test_index.py
@@ -5,7 +5,7 @@ class TestRegister(SeleniumTest):
     # assume that we passed registration and should be on /dashboard/
     def test_successful_registration(self):
         driver = self.driver
-        self.assertEqual(self.base_url+'/dashboard/', driver.current_url)
+        self.assertEqual(self.base_url+'/dashboard/French', driver.current_url)
         self.assertEqual(
             "{user_name}".format(user_name=self.user_id),
             driver.find_elements_by_class_name(
